db73fcd35c1a09cb959e5e7ac370c66cf3e7b548
shivaramganesan <sgane014@ucr.edu>
2023-11-02 18:53:38 -0700

    scala, spark dependencies added | Parquet file reading template added

--- src/main/java/edu/ucr/cs/cs226/App.java
+++ src/main/java/edu/ucr/cs/cs226/App.java
@@ -3,6 +3,9 @@ package edu.ucr.cs.cs226;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.SparkConf;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
 
 /**
  * Hello world!
@@ -12,13 +15,23 @@ public class App
 {
     public static void main( String[] args )
     {
-        SparkConf conf = new SparkConf().setAppName("test").setMaster("local");
+        SparkConf conf = new SparkConf().setAppName("NYC_Trips").setMaster("local");
         JavaSparkContext sc = new JavaSparkContext(conf);
 
-        JavaRDD<String> lines = sc.textFile(args[0]);
-        JavaRDD<Integer> lineLengths = lines.map(s -> s.length());
-        int totalLength = lineLengths.reduce((a, b) -> a + b);
-        System.out.printf("Total length is %d\n", totalLength);
+        SparkSession spark = SparkSession.builder().getOrCreate();
+        String filePath = "hdfs://localhost:9000/input/2023/2023/jan.parquet";
+//
+        Dataset<Row> parquetFile = spark.read().parquet(filePath);
+//
+        parquetFile.createOrReplaceTempView("tempView");
+        Dataset<Row> rows = spark.sql("select * from tempView");
+        rows.show();
+
+//        JavaRDD<String> lines = sc.textFile("hdfs://localhost:9000/input/2023/jan.parquet");
+//        System.out.println("Test Reading done here :: " + lines.count());
+//        JavaRDD<Integer> lineLengths = lines.map(s -> s.length());
+//        int totalLength = lineLengths.reduce((a, b) -> a + b);
+//        System.out.printf("Total length is %d\n", totalLength);
 
     }
 }
