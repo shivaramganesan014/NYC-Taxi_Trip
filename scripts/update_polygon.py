b0b2d18efba1f00756191a17f937df497c8d883f
shivaramganesan <sgane014@ucr.edu>
2023-11-27 14:37:12 -0800

    coordinates from polygon added in python script

--- scripts/update_polygon.py
+++ scripts/update_polygon.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import pandas as pd
+from shapely.geometry import MultiPoint
 
 def convert(val):
 	val = val.split(" ")
@@ -26,6 +27,18 @@ def transform(x):
 	
 	# return res
 
+def get_lat_lng(points, zone):
+	# print(zone+"::"+str(len(points)))
+	res = []
+	count = 0
+	for p in points:
+		if(len(p) > 1):
+			t = (p[0], p[1])
+			res.append(t)	
+	pts = MultiPoint(res)
+	return pts.centroid
+
+
 def update_polygon(filepath):
 
 	for f in os.listdir(filepath):
@@ -38,7 +51,24 @@ def update_polygon(filepath):
 					df2 = pd.read_csv('./polygons.csv')
 					df = pd.merge(df1, df2, left_on='locationid', right_on='OBJECTID', how='left').drop("OBJECTID", axis=1)
 					df["the_geom"] = df["the_geom"].apply(transform)
+					df['lat'] = pd.Series(dtype='float')
+					df['lng'] = pd.Series(dtype='float')
+					for i in range(0, len(df["the_geom"])):
+						coords = get_lat_lng(df["the_geom"][i], df["zone"])
+						# if(len(coords) > 0):
+							# print(coords[0])
+						if(coords):
+							# print(coords.x)
+							# print(coords.y)
+							df.loc[i, "lat"] = coords.y
+							df.loc[i, "lng"] = coords.x
+							# df['lat'][i] = coords.x
+							# df['lng'][i] = coords.y
+							
+					# df["lng"] = df["the_geom"].apply(get_lat_lng)
+					# pts = get_lat_lng(df["the_geom"].apply)
 					df.to_csv(retpath+"busiest_t.csv")
+
 					break
 		else:
 			print("not dir")
